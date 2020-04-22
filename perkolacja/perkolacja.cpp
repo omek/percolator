8d540cf91deab325086cf72420485dcd37269a23
omek <omegaarma@gmail.com>
2020-04-22 17:47:35 +0200

    dodano pomiar czasu wykonania symulacji

--- perkolacja/perkolacja.cpp
+++ perkolacja/perkolacja.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <time.h>
+#include <chrono>
 #include "SiatkaProstokatna.h"
 #include "MonteCarlo.h"
 
@@ -12,14 +13,24 @@ int main()
 	int iloscSymulacji = 100;
 	std::vector<int>iPerkolacja;
 	std::vector<int>WynikiSymulacji;
+
 	for (int i = 0; i < 100; i += 10){
 		iPerkolacja.push_back(i);
 	}
+
 	MonteCarlo s(iloscSymulacji, iPerkolacja, x, y);
+
+	auto start = std::chrono::high_resolution_clock::now();
 	WynikiSymulacji = s.Symulacja();
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
 	for (int i = 0; i < iPerkolacja.size(); i++) {
 		std::cout <<"Dla "<<iPerkolacja[i]<<"% perkolacji "<< WynikiSymulacji[i]<<"% symulacji powiodlo sie." << std::endl;
 	}
+	std::cout << std::endl;
+	std::cout << "Czas trwania symulacji: " << dur.count() <<" milisekund"<< std::endl;
+
 	std::cin.get();
 	return 0;
 }
