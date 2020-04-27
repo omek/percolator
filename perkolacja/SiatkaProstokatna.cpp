ff7873deceaf92c0ecc2ca16d3d7e97bc48d9de0
omek <omegaarma@gmail.com>
2020-04-27 12:17:53 +0200

    ostatnie poprawki, wersja ostateczna

--- perkolacja/SiatkaProstokatna.cpp
+++ perkolacja/SiatkaProstokatna.cpp
@@ -23,42 +23,45 @@ void SiatkaProstokatna::pokaz() {
 	std::cout << std::endl;
 }
 
-void SiatkaProstokatna::sprawdzRekurencja(std::vector<std::vector<bool>> &siatka_bazowa, std::vector<std::vector<bool>> &siatka_perkolacji, int h, int w) {
+bool SiatkaProstokatna::sprawdzRekurencja(std::vector<std::vector<bool>> &siatka_bazowa, std::vector<std::vector<bool>> &siatka_perkolacji, int h, int w) {
 	siatka_perkolacji[h][w] = true;
+
+	if (h == Height - 1)
+		return true;
+	
 	if (h < Height - 1) {
 		if (siatka_bazowa[h + 1][w] && !siatka_perkolacji[h + 1][w]) {
-			sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h + 1, w);
+			if (sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h + 1, w))
+				return true;
 		}
 	}
 	if (w < Width - 1) {
 		if (siatka_bazowa[h][w + 1] && !siatka_perkolacji[h][w + 1]) {
-			sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h, w + 1);
+			if (sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h, w + 1))
+				return true;
 		}
 	}
 	if (w > 0) {
 		if (siatka_bazowa[h][w - 1] && !siatka_perkolacji[h][w - 1]) {
-			sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h, w - 1);
+			if (sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h, w - 1))
+				return true;
 		}
 	}
 	if (h > 0) {
 		if (siatka_bazowa[h - 1][w] && !siatka_perkolacji[h - 1][w]) {
-			sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h - 1, w);
+			if (sprawdzRekurencja(siatka_bazowa, siatka_perkolacji, h - 1, w))
+				return true;
 		}
 	}
+	return false;
 }
 
 bool SiatkaProstokatna::sprawdz(std::vector<std::vector<bool>> sP) {
-
+	bool test = false;
 	for (int i = 0; i < siatka_bazowa[0].size(); i++)
-		if (siatka_bazowa[0][i])
-			sprawdzRekurencja(siatka_bazowa, sP, 0, i);
-
-
-	for (int j = 0; j < Width - 1; j++) {
-		if (sP[Height - 1][j])
-			return true;
-	}
-	return false;
+		if (siatka_bazowa[0][i] && !test)
+			test = sprawdzRekurencja(siatka_bazowa, sP, 0, i);
+	return test;
 }
 
 
--- perkolacja/SiatkaProstokatna.h
+++ perkolacja/SiatkaProstokatna.h
@@ -6,7 +6,7 @@ public:
 	SiatkaProstokatna(int w, int h);
 	~SiatkaProstokatna();
 	bool sprawdz(std::vector<std::vector<bool>>);
-	void sprawdzRekurencja(std::vector<std::vector<bool>> &siatka_bazowa,std::vector<std::vector<bool>> &siatka_perkolacji, int h,int w);
+	bool sprawdzRekurencja(std::vector<std::vector<bool>> &siatka_bazowa,std::vector<std::vector<bool>> &siatka_perkolacji, int h,int w);
 	void pokaz();
 	void losuj(int prawdopodobienstwo);
 private:
